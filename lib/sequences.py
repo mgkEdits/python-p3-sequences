60d70081b06f438a13abb0ddbdabd7c0dff90262
mgkEdits <elijahnyasiando.mn@gmail.com>
2023-11-21 15:19:57 +0300

    cmpltd lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+        fibonacci_sequence = []
+        a, b = 0, 1
+
+        if length == 0:
+          print([])
+          return
+        elif length == 1:
+          print([a])
+          return
+        elif length == 2:
+          print([a,b])
+          return
+
+        for _ in range(length):
+          fibonacci_sequence.append(a)
+          a, b = b, a + b
+
+        print(fibonacci_sequence)
+        pass
+
+
+print_fibonacci(10)
